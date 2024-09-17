f820aed622aa2f1636ff39bc2e14788ecbf2ac16
FerUgalde <fernanda.ugalde@uabc.edu.mx>
2024-09-17 16:29:34 -0700

    errors

--- sime-f/src/app/views/student/Comments/Register/StudentCommentRegister.tsx
+++ sime-f/src/app/views/student/Comments/Register/StudentCommentRegister.tsx
@@ -8,7 +8,8 @@ import { useEffect, useState } from 'react';
 import { StudentService } from '../../../../services/students/StudentsService';
 import { StudentCommentsService } from '../../../../services/students/StudentCommentsService';
 import { User } from '../../../../interfaces/user/User';
-import { UsersService } from '../../../../services/users/UsersService';
+import { ToastContainer } from 'react-toastify';
+import TextField from '../../../../components/shared/FormInputs/TextField';
 
 const StudentCommentsRegister = () => {
 
@@ -17,9 +18,6 @@ const StudentCommentsRegister = () => {
     register,
     handleSubmit,
     formState: { errors },
-    control,
-    getValues,
-    watch
   } = useForm<StudentComments>();
   const navigate = useNavigate();
   const { setLoading } = useLoader();
@@ -30,6 +28,14 @@ const StudentCommentsRegister = () => {
   // load user
   const loadUser = async () => {
     let user = sessionStorage.getItem('user');
+    console.log(user);
+    if (user) {
+      let userObj = JSON.parse(user);
+      console.log(userObj);
+      setUserComments(userObj);
+    } else {
+      console.log('not user');
+    }
   };
 
   const loadStudent = async (studentId: number) => {
@@ -49,7 +55,9 @@ const StudentCommentsRegister = () => {
     console.log(data);
     const formData = new FormData();
     formData.append('comment', data.comment);
-    formData.append('student_id', studentComments?.id?.toString() || '');
+    formData.append('by', userComments?.first_name + ' ' + userComments?.last_name);
+    formData.append('userRoleCreator', userComments?.role.toString() || '');
+    formData.append('idStudent', studentComments?.id?.toString() || '');
 
     const resp = await StudentCommentsService.register(formData, studentComments?.id || 0);
     console.log(studentComments);
@@ -63,6 +71,10 @@ const StudentCommentsRegister = () => {
   };
 
   useEffect(() => {
+    loadUser();
+  }, []);
+
+  useEffect(() => {
     if (!id) return;
     let studentId = parseInt(id);
     loadStudent(studentId);
@@ -71,6 +83,38 @@ const StudentCommentsRegister = () => {
   return (
     <div className='student-comments'>
       <h1>Agregar Comentario</h1>
+      <div className='form'>
+        <div className="row mb-2">
+          <div className="col-4 btn-edit">
+            <button className='btn btn-secondary' onClick={() => navigate(`/student/overview/${studentComments?.id}`)}>Volver</button>
+          </div>
+        </div>
+        <div className="row mb-2 mt-3">
+          <hr className="border border-secondary border-1 opacity-75" />
+        </div>
+        <div className='container-fluid-mb-3 form-group'>
+          <div className="mb-4">
+            <TextField
+              label={'Comentario'}
+              field={'comment'}
+              register={register}
+              type='text'
+              rules={{ required: 'Este campo es requerido' }}
+              errors={errors}
+              multiLine={true}
+            />
+          </div>
+          <div className="row mb-2 mt-3">
+            <hr className="border border-secondary border-1 opacity-75" />
+          </div>
+          <div className="row">
+            <div className="col-8">
+              <button className="btn btn-primary xl" onClick={handleSubmit((data) => handleCreate(data))}>Añadir Comentario</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
     </div>
   );
 }
